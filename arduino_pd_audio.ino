5d929e612a32979a4054e8cc51e634865873cd99
nurkouu <143274560+nurkouu@users.noreply.github.com>
2026-09-16 18:07:28 -0400

    Update arduino_pd_audio.ino

--- arduino_pd_audio.ino
+++ arduino_pd_audio.ino
@@ -41,3 +41,68 @@ void loop() {
     //Serial.println(23); // Envoie la valeur et ajoute automatiquement le saut de ligne final
 
 }
+
+
+
+/*
+
+#include <Arduino.h>
+#include <Chrono.h>
+#include <Bounce2.h>
+ 
+#define BROCHE_DEL 3
+#define BROCHE_BOUTON 2
+ 
+#define INTERVALLE 500
+ 
+Bounce2::Button bouton = Bounce2::Button();
+Chrono minuterieDel;
+ 
+bool etatDel = LOW;
+bool clignotementActif = false;
+ 
+void setup()
+{
+    // Configuration de la DEL
+    pinMode(BROCHE_DEL, OUTPUT);
+    digitalWrite(BROCHE_DEL, etatDel);
+ 
+    // Configuration du bouton
+    bouton.attach(BROCHE_BOUTON, INPUT_PULLUP);
+    bouton.setPressedState(LOW);
+}
+ 
+void loop()
+{
+    // Mise à jour du bouton
+    bouton.update();
+ 
+    // Gestion de l'événement de pression
+    if (bouton.pressed())
+    {
+        if (clignotementActif) {
+            clignotementActif = false;
+        } else {
+             clignotementActif = true;
+        }
+    }
+ 
+    // Gestion indépendante du clignotement
+    if (clignotementActif) {
+        if ( minuterieDel.hasPassed(INTERVALLE))
+        {
+            minuterieDel.restart();
+ 
+            if ( etatDel == 0 ) {
+                etatDel = 1;
+            } else {
+                etatDel = 0;    
+            }
+           
+        }
+    }
+ 
+    digitalWrite(BROCHE_DEL, etatDel);
+}
+
+*/
